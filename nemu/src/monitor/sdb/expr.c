cfb8fb46bf7ef98f55460a4bf8cf0d3225a0ed78
huzhisheng <1216265845@qq.com>
2021-10-09 22:14:14 +0800

    PA1阶段2初步完成

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -4,12 +4,15 @@
  * Type 'man regex' for more information about POSIX regex functions.
  */
 #include <regex.h>
+#include <common.h>
+
+static void test_expr();
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
+  TK_NOTYPE = 256, TK_EQ,TK_PLUS,
 
   /* TODO: Add more token types */
-
+  TK_NUM,TK_MINUS,TK_MULTI,TK_DIV,TK_LK,TK_RK,
 };
 
 static struct rule {
@@ -17,13 +20,20 @@ static struct rule {
   int token_type;
 } rules[] = {
 
+  
+
+  {" +", TK_NOTYPE},    // spaces
+  {"\\+", TK_PLUS},         // plus
+  {"==", TK_EQ},        // equal
   /* TODO: Add more rules.
    * Pay attention to the precedence level of different rules.
    */
-
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ},        // equal
+  {"^[1-9][0-9]*", TK_NUM},
+  {"-", TK_MINUS},
+  {"\\*", TK_MULTI},
+  {"/", TK_DIV},
+  {"\\(", TK_LK},
+  {"\\)", TK_RK},
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -45,6 +55,9 @@ void init_regex() {
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
     }
   }
+
+  // TODO: 加载gen_expr产生的文件, 并进行测试
+  test_expr();
 }
 
 typedef struct token {
@@ -80,7 +93,12 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          default: TODO();
+          default: {
+            tokens[nr_token].type = rules[i].token_type;
+            memcpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            nr_token++;
+          }
         }
 
         break;
@@ -96,15 +114,169 @@ static bool make_token(char *e) {
   return true;
 }
 
+int check_parentheses(int p, int q){
+  if(tokens[p].type != TK_LK) return false;
+
+  int left_numbers = 0; // 左括号的个数
+
+  int i;
+  for(i=p; i<=q; i++){
+    if(tokens[i].type == TK_LK){
+      left_numbers++;
+    }
+    else if(tokens[i].type == TK_RK){
+      left_numbers--;
+      if(i < q && left_numbers <= 0){
+        return false;
+      }
+    }
+  }
+  if(left_numbers == 0) return true;
+  else return false;
+}
+
+int find_main_op(int p, int q){
+  int in_brackets = 0;
+
+  int ret = -1;
+
+  int i;
+  for(i = p; i <= q; i++){
+    switch (tokens[i].type)
+    {
+    case TK_PLUS:
+    case TK_MINUS:
+      {
+        if(!in_brackets){
+          ret = i;
+        }
+      }
+      break;
+    case TK_MULTI:
+    case TK_DIV:
+      {
+        if(ret == -1 && !in_brackets){
+          ret = i;
+        }
+      }
+      break;
+    case TK_LK:
+      {
+        in_brackets++;
+      }
+      break;
+    case TK_RK:
+      {
+        in_brackets--;
+      }
+      break;
+    default:
+      break;
+    }
+  }
+  return ret;
+}
+
+word_t eval(int p, int q) {
+  printf("eval: %d, %d\n", p, q);
+  if (p > q) {
+    /* Bad expression */
+    printf("err1\n");
+    return -1;  // TODO: 返回值全1代表出错, 这步可能会有隐患
+  }
+  else if (p == q) {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+    if(tokens[p].type != TK_NUM){ // 非数字
+      printf("err2\n");
+      return -1;
+    }
+    return atoll(tokens[p].str);
+  }
+  else if (check_parentheses(p, q) == true) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(p + 1, q - 1);
+  }
+  else {
+    int op_index = find_main_op(p, q);
+    if(op_index == -1){
+      printf("err3\n");
+      return -1;
+    }
+    word_t val1 = eval(p, op_index - 1);
+    word_t val2 = eval(op_index + 1, q);
+
+    switch (tokens[op_index].type) {
+      case TK_PLUS: return val1 + val2;
+      case TK_MINUS: return val1 - val2;
+      case TK_MULTI: return val1 * val2;
+      case TK_DIV: {
+        if(val2 == 0) return 0; // TODO: 除以0则代表是非法表达式, 暂时就认为结果为0
+        return val1 / val2;
+      }
+      default: printf("err4\n");return -1;
+    }
+  }
+}
+
 
 word_t expr(char *e, bool *success) {
+  printf("开始计算表达式:%s\n",e);
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
 
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  // TODO: 打印所有识别的东西
+  for(int i=0; i<nr_token; i++){
+    printf("第%d个token: %s\n", i, tokens[i].str);
+  }
 
-  return 0;
+  /* TODO: Insert codes to evaluate the expression. */
+
+  word_t ret_val = eval(0, nr_token-1);
+
+  if(ret_val == -1){
+    printf("Error in expr.\n");
+    return 0;
+  }
+
+  return ret_val;
+}
+
+// TODO: 加载gen_expr产生的文件, 并进行测试
+static void test_expr(){
+  char filename[] = "/tmp/.code.input"; //文件名
+  char *temp_p;
+  FILE *fp; 
+  char StrLine[1024]; //每行最大读取的字符数
+  if((fp = fopen(filename,"r")) == NULL){ //判断文件是否存在及可读
+      printf("Error in test_expr.\n"); 
+      return; 
+  } 
+
+  while (!feof(fp)) 
+  { 
+      temp_p = fgets(StrLine,1024,fp); //读取一行
+      if(temp_p == NULL){
+        printf("Error in test_expr.\n"); 
+        return; 
+      }
+
+      StrLine[strlen(StrLine) - 1] = '\0'; // 必须要忽略最后的换行符
+
+      char *result_ptr = strtok(StrLine, " ");
+      char *expr_ptr = strtok(NULL, " ");
+      bool if_success = false;
+      word_t expect_result = atoll(result_ptr);
+      word_t actual_result = expr(expr_ptr, &if_success);
+
+      printf("%ld, 计算结果:%ld\n", expect_result, actual_result);
+  } 
+  fclose(fp); //关闭文件
+  return; 
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -7,6 +7,9 @@
 
 // this should be enough
 static char buf[65536] = {};
+// TODO: 添加一个buf长度变量
+static int buf_len = 0;
+
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
@@ -16,8 +19,44 @@ static char *code_format =
 "  return 0; "
 "}";
 
+// TODO:
+static int choose(int num_choices)
+{
+  return rand()%num_choices;    //用rand产生随机数并设定范围
+}
+// TODO:
+static void gen_num(){
+  int rand_num = choose(1000);
+  char num_buf[5];
+  sprintf(num_buf, "%d", rand_num);
+  int num_str_len = strlen(num_buf);
+  memcpy(buf+buf_len, num_buf, num_str_len);
+  buf_len += num_str_len;
+}
+// TODO:
+static void gen(char e){
+  buf[buf_len++] = e;
+  return;
+}
+// TODO:
+static void gen_rand_op(){
+  switch (choose(5)) {
+    case 0: buf[buf_len++] = '+'; break;
+    case 1: buf[buf_len++] = '-'; break;
+    case 2: buf[buf_len++] = '*'; break;
+    default: buf[buf_len++] = '/'; break;
+  }
+}
+
+// TODO:
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  switch (choose(5)) {
+    case 0:
+    case 1:
+    case 2: gen_num(); break;
+    case 3: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -29,7 +68,12 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    buf_len = 0;  // TODO: 清零buf_len
+
     gen_rand_expr();
+
+    if(buf_len >= 65500) continue;  // TODO: 生成的表达式太长, 跳过
+    buf[buf_len] = '\0';
 
     sprintf(code_buf, code_format, buf);
 
